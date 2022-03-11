e53be37b8e55f9e4e1cf281eee81cc7a69c0fdbc
Rafael <rafaelsantosgoncalves5@gmail.com>
2022-03-10 21:23:06 -0300

    add projeto

---
+++ js.js
@@ -0,0 +1,7 @@
+var listaFilmes = ["http://www.setcenas.com.br/wp-content/uploads/2016/12/rocky-poster.jpg",
+"https://br.web.img3.acsta.net/medias/nmedia/18/90/57/96/20121842.jpg",
+"https://saladadecinema.com.br/wp-content/uploads/2021/06/Aranha-560x700.jpg"]
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] +">")
+}
